e91da34db40bde83e9e692dc42728c6755e4adca
caracalla <eahduarte@inf.ufrgs.br>
2024-12-09 16:36:11 -0300

    update and includes to  main

--- etapa4/src/main.c
+++ etapa4/src/main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include "stack.h"
+#include "errors.h"
+
 extern int yyparse(void);
 extern int yylex_destroy(void);
 void *arvore = NULL;
+TableStack *stack = NULL;
+
 void exporta (void *arvore);
 int main (int argc, char **argv)
 {
